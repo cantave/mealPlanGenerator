0e3703cce7084e423b7f5287ef55d915b547115c
cantave <32780807+cantave@users.noreply.github.com>
2024-11-25 21:46:30 -0600

    updated security features

--- src/main/java/com/cantave/mealPlanGenerator/config/SecurityConfig.java
+++ src/main/java/com/cantave/mealPlanGenerator/config/SecurityConfig.java
@@ -1,23 +1,30 @@
 package com.cantave.mealPlanGenerator.config;
 
+import com.cantave.mealPlanGenerator.filter.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(withDefaults())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
@@ -25,11 +32,18 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/cantave/mealPlanGenerator/controllers/AuthController.java
+++ src/main/java/com/cantave/mealPlanGenerator/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.cantave.mealPlanGenerator.controllers;
 
+import com.cantave.mealPlanGenerator.dto.LoginRequest;
 import com.cantave.mealPlanGenerator.dto.RegisterRequest;
 import com.cantave.mealPlanGenerator.models.User;
 import com.cantave.mealPlanGenerator.services.UserService;
@@ -9,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,11 +31,11 @@ public class AuthController {
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest loginRequest) throws Exception{
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username, loginRequest.password));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
         } catch (Exception ex) {
             throw new Exception("Invalid username or password");
         }
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.username);
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
         final String jwt = jwtUtil.generateToken(userDetails.getUsername());
         return jwt;
     }
@@ -45,9 +45,4 @@ public class AuthController {
         User user = userService.registerUser(registerRequest);
         return "User registered successfully: " + user.getUsername();
     }
-}
-
-class LoginRequest {
-    public String username;
-    public String password;
 }
